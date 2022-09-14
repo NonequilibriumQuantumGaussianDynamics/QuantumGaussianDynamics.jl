430a55b730cdfc43ea86fa130a79365f6805c528
Lorenzo Monacelli <lorenzo.monacelli@epfl.ch>
2022-09-14 18:04:55 +0200

    Added new functions

--- src/QuanumGaussianDynamics.jl
+++ src/QuanumGaussianDynamics.jl
@@ -3,6 +3,36 @@ module QuanumGaussianDynamics
 using LinearAlgebra
 
 """
+Some guide on the units
+
+We adopt Hartree atomic units, where the energy is expressed in mHa (convenient for phonons).
+Therefore each derived units (like forces and times) are appropriately rescaled.
+"""
+
+const SMALL_VALUE :: Float64 = 1e-8
+const CONV_FS :: Float64 = 0.04134137333518211  # femtoseconds
+const CONV_EV :: Float64 = 3.674932217565499e-5
+const CONV_ANGSTROM :: Float64 = 1.8897261246257702
+
+"""
+Here information about the dynamics are stored, 
+like integration algorithm, time_step, kong_liu ratio 
+and so on, so forth. 
+"""
+struct Dynamics{T <: AbstractFloat}
+    dt :: T   #In femtosecodns
+    total_time :: T # In femtosecodns
+    algorithm :: String
+    kong_liu_ratio :: T
+    verbose :: Bool
+
+    # Save the data each
+    save_filename :: String 
+    save_correlators :: Bool 
+    save_each :: Int32 
+end
+
+"""
 Info about the wigner distribution
 
 Note that all the variable here are with a tilde (mass rescaled)
@@ -16,19 +46,58 @@ struct WignerDistribution{T<: AbstractFloat}
     RR_corr :: Matrix{T}
     PP_corr :: Matrix{T}
     RP_corr :: Matrix{T}
+
+    # Eigenvalues and eigenvectors of the current Y matrix 
+    λs :: Vector{T}
+    λs_vect :: Matrix{T}
 end
 
 
 struct Ensemble{T <: AbstractFloat}
     original_RR_corr :: Matrix{T} # All quantities with the tilde
     original_R_av :: Matrix # All these quantities are with the tilde
+
+    # Eigenvalues and eigenvectors of the Y matrix that generated the ensemble
     λs_inv :: Vector{T}
+    λs_vect :: Matrix{T}
+
     positions :: Matrix{T}  # Positions are multiplied by the squareroot of the masses
+    forces :: Matrix{T} # Forces are divided by the squareroot of masses
+    # index i, j means configuration j, coordinate i
     n_configs :: Int32
     weights :: Vector{T}
+
+    masses :: Vector{T}
+    unit_cell :: Matrix{T}
 end 
+
+"""
+Remove acoustic sum rule from eigenvalue and eigenvectors
+"""
+function remove_translations(vectors, values)
+    not_trans_mask =  values .> SMALL_VALUE
+
+    @assert sum(not_trans_mask) == 3   """
+Error, the expected number of acustic modes is 3
+       got $(sum(not_trans_mask)) instead.
+"""
+
+    new_values = values[ not_trans_mask ]
+    new_vectors = vectors[:, not_trans_mask]
+
+    return new_vectors, new_values
+end
+
+
+"""
+Impose the ASE projecting out the translations
+"""
+function apply_ASR!(matrix :: Matrix{T}, masses :: Vector{T}) where {T <: AbstractFloat}
+    mass_array = zeros(T, size(matrix, 1))
+end
 
 include("time_evolution.jl")
 include("ensemble.jl")
+include("dynamics.jl")
 
 end # module QuanumGaussianDynamics
--- src/ensemble.jl
+++ src/ensemble.jl
@@ -4,23 +4,77 @@ Update the weights of the ensemble according with the new wigner distribution
 """
 function update_weights!(ensemble:: Ensemble{T}, wigner_distribution :: WignerDistribution{T}) where {T :< AbstractFloat}
     # Prepare the normalization rescale
-    λs = get_λs(wigner_distribution.RR_corr)
-    ensemble.weights .= sqrt(λs' * ensemble.λs_inv)
+    lambda_eigen = eigen(wigner_distribution.RR_corr)
 
-    # TODO: It is possible to get everything more efficient and avoid these two inversions
-    #       by simply storing also the eigenvectors and projecting delta into the space on which Upsilon is diagonal
-    upsilon_new = inv(wigner_distribution.RR_corr)
-    upsilon_old = inv(wigner_distribution)
-    delta_new = ensemble.positions - wigner_distribution.R_av
-    delta_old = ensemble.positions - ensemble.original_R_av
+    # Discard the sum rule
+    λvects, λs = remove_translations(lambda_eigen.vectors, lambda_eigen.values)
+    ensemble.weights .= sqrt(λs' * ensemble.λs_inv)
+    wigner_distribution.λs .= λs 
+    wigner_distribution.λs_vect .= λs_vect
+
+
+    delta_new = zeros(T, wigner_distribution.n_atoms * 3)
+    delta_old = zeros(T, wigner_distribution.n_atoms * 3)
+    u_new = zeros(T, length(λs))
+    u_old = zeros(T, length(λs))
+
+
+    @views for i = 1:ensemble.n_configs
+        delta_new .=  ensemble.positions[:, i] .- wigner_distribution.R_av
+        delta_old .=  ensemble.positions[:, i] .- ensemble.original_R_av
+
+        u_new .= lambda_eigen.vectors' * delta_new 
+        u_new ./= sqrt(λs)
+        u_old .= (ensemble.λs_vect' * delta_old) 
+        u_old .*= sqrt(ensemble.λs_inv)
+
+        numerator = - 0.5 * (u_new' * u_new)
+        denominator = - 0.5 *  (u_old' * u_old)
+        ensemble.weights[i] *= exp( numerator - denominator)
+    end
 end 
 
 
 """
+Evaluate the average dv_dr and d2v_dr2 from the ensemble and the wigner distribution.
+
+The weights on the ensemble should have been already updated.
+"""
+function get_averages!(dv_dr :: Vector{T}, d2v_dr2 :: Matrix{T}, ensemble :: Ensemble{T}, wigner_distribution :: WignerDistribution{T}) where {T <: AbstractFloat}
+    dv_dr .= sum(ensemble.forces, dims = 2)
+
+    n_modes = length(wigner_distribution.λs)
+    delta = zeros(T, wigner_distribution.n_atoms * 3)
+    d2v_dr2_tmp = zeros(T, (3*wigner_distribution.n_atoms, 3*wigner_distribution.n_atoms))
+    d2v_dr2 .= 0
+    @views for i = 1 : ensemble.n_configs
+        delta .= ensemble.positions[:, i] - wigner_distribution.R_av
+        mul!(d2v_dr2, delta, ensemble.forces', 1.0, 1.0)
+    end
+
+    e_tilde = copy(wigner_distribution.λs_vect)
+    for i = 1 : n_modes
+        e_tilde[:, i] .*= wigner_distribution.λs
+    end 
+
+    # Now multiply by upsilon
+    mul!(d2v_dr2_tmp, e_tilde', d2v_dr2)
+    mul!(d2v_dr2, wigner_distribution.λs_vect, d2v_dr2)
+
+    # Impose permutation symmetry
+    d2v_dr2 .+= d2v_dr2'
+    d2v_dr2 ./= 2
+
+    # TODO! Impose the acoustic sum rule and the symmetries
+end 
+
+"""
 Initialize the ensemble
 """
-function init_ensemble!(ensemble :: Ensemble)
-    ensemble.λs_inv = 1 ./ get_λs(ensemble.original_RR_corr)
+function init_ensemble!(ensemble :: Ensemble{T}) where {T <: AbstractFloat}
+    # Discard the sum rule (TODO, improve the sum rule on the eigenvectors)
+    ensemble.λs_vect, λs = remove_translations(lambda_eigen.vectors, lambda_eigen.values)
+    ensemble.λs_inv = 1 ./ λs
 end
 
 
@@ -28,3 +82,8 @@ function get_λs(RR_corr :: Matrix{T}) where {T <: AbstractFloat}
     eigvals:: T = eigvals(Hermitian(RR_corr))
     return eigvals
 end 
+
+
+# TODO: add a function to load and save the ensemble on disk
+function load_ensemble!(ensemble :: Ensemble{T}, path_to_json :: String) where {T <: AbstractFloat}
+end
