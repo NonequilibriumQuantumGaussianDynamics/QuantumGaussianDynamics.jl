0f26709654b051bd51eaebf5caa10fd7cdd3aa79
flibbi <francesco.libbi93@.com>
2025-09-04 15:34:59 +0200

    removing TDSCHA name

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, TDSCHA
-using Unitful
+using Documenter, QuantumGaussianDynamics
 
-makedocs(sitename="TDSCHA.jl")
+makedocs(
+    sitename = "QuantumGaussianDynamics.jl",
+    format   = Documenter.HTML(),
+    modules  = [QuantumGaussianDynamics],
+    clean    = true,
+    strict   = true,          # fail the build on doc warnings (nice for CI)
+    pages    = [
+        "Home" => "index.md",
+        "API"  => "api.md",
+    ],
+)
+
+# For GitHub Pages deployment
+deploydocs(
+    repo = "https://github.com/NonequilibriumQuantumGaussianDynamics/QuantumGaussianDynamics.jl",
+    devbranch = "main",       # or "master"
