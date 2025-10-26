5d1ed8f673d441046c98069e9a28ed622f26902d
flibbi <francesco.libbi93@.com>
2025-10-25 22:19:03 -0400

    main

--- examples/H2/main.jl
+++ examples/H2/main.jl
@@ -14,7 +14,7 @@ using DelimitedFiles
 @pyimport ase.calculators.emt as emt
 
 
-# Load the dyn corresponding to the equilibrium structure of a SSCHA calculation
+#1) Load the equilibrium structure and ensemble
 TEMPERATURE = 0.0
 dyn_file = "final_result"
 ens_file = "sscha_ensemble"
@@ -23,61 +23,41 @@ ndyn = 1
 py_ensemble, dyn = equilibrium_ensemble(TEMPERATURE, dyn_file, ens_file, ndyn, ens_bin)
 
 
-"""
-Inputs
-dt: discretization time, femtoseconds
-total_time: run time, femtoseconds
-algorithm: evolution algorithm
-kong_liu_ratio: use 1.0, lower values may not conserve the energy
-evolve_correlators: use true
-N: number of configurations
-seed: seed for the calculations. If you do not want to specify any seed, use seed=0
-correlated: whether the dynamics is correlated or not. 
-"""
-method = "semi-implicit-verlet" # use this one
+#2) Dynamics settings
+method = "generalized-verlet" # most accurate
 settings = Dynamics(
-    dt = 0.1,
-    total_time = 10.0,
-    algorithm = method,
-    kong_liu_ratio = 1.0,
-    verbose = true,
-    evolve_correlators = true,
-    save_filename = method,
-    save_correlators = true,
-    save_each = 1,
-    N = 100,
-    seed = 1254,
-    correlated = true,
+    dt = 0.1,                     # time step, fs
+    total_time = 10.0,            # total simulation time, fs
+    algorithm = method,           # integration scheme
+    kong_liu_ratio = 1.0,         # kong-liu ratio, reweighting
+    verbose = true,               # verbosity of the output
+    evolve_correlators = true,    # evolve <RR>, <PP>, <RP> matrices
+    save_filename = method,       # filename
+    save_correlators = true,      # save the full output
+    save_each = 1,                # output step 
+    N = 100,                      # number of stochastic configurations
+    seed = 1254,                  # seed for reproducibility (0 = no seed)
+    correlated = true,            # correlated approach
 )
-rho = init_from_dyn(dyn, Float64(TEMPERATURE), settings)
+rho = init_from_dyn(dyn, TEMPERATURE, settings)
 ensemble = init_ensemble_from_python(py_ensemble, settings)
 
 
-# Specify here the ASE calculator
+# 3) ASE calculator
 calculator = emt.EMT()
 crystal = init_calculator(calculator, rho, ase.Atoms)
 
 
-# Electric field
-# If you do not want to apply any field, use fake_field, like this. Otherwise, prepare a fake ph.out to read the effective charges and the dielectric constant
-efield = fake_field(rho.n_atoms)
+# 4) Electric field
+A = 3000.0 #kV/cm 
+freq = 2.4 #THz
+t0 = 1875.0 #fs
+sig = 468.0 #fs
+edir = [0,0,1.0] 
+field_fun = pulse(A, freq, t0, sig)
+Zeff, eps = fake_dielectric_constant(rho.n_atoms)
 
-# If you want to apply the pulse, specify the parameters
-# Equation of the pulse: E(t)=A*cos(2\pi*freq*t)*exp(-(t-t0)^2/(2*sig^2))
-#A = 3000.0 
-#freq = 2.4 #THz
-#t0 = 1875.0 #fs
-#sig = 468.0 #fs
-#edir = [0,0,1.0] #Polarization of the field, must have norm=1
-#field_fun = deepcopy(QuantumGaussianDynamics.pulse)
-#field_f = t -> field_fun(t,A,freq,t0,sig)
+efield = QuantumGaussianDynamics.ElectricField(fun = field_fun, Zeff = Zeff, edir=edir, eps = eps)
 
-# Read effective charges and dielectric tensor from ph.out
-#Zeff, eps = QuantumGaussianDynamics.read_charges_from_out!("ph.out",  rho)
-#efield = QuantumGaussianDynamics.ElectricField(fun = field_f, Zeff = Zeff, edir=edir, eps = eps)
-
-# Displacement from equilbrium, optional
-rho.P_av[1] += 0.01 #sqrt(Ry)
-
-# Run!
+# 5) Run!
 integrate!(rho, ensemble, settings, crystal, efield)
